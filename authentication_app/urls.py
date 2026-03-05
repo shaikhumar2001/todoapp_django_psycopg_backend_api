68c3d5210224829c0641636956d5dde9a1b7a85f
shaikhumar2001 <shaikhumar06122001@gmail.com>
2026-03-05 15:58:29 +0530

    feat: add registration endpoint to authentication URLs

--- authentication_app/urls.py
+++ authentication_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .apis.auth_view import login
+from .apis.auth_view import login, register
 
 urlpatterns = [
-    path("login/", login, name="login")
+    path("login/", login, name="login"),
+    path("register/", register, name="register"),
 ]
